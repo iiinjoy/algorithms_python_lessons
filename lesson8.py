baff20be03c8dcbe9f56ab77c4b7c9df1faae4c3
Aynur Ishbuldin <iiinjoy@gmail.com>
2020-03-05 23:34:44 +0500

    lesson8

---
+++ lesson8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+class HashTable:
+
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        # djb2
+        acc = 5381
+        for c in value:
+            code = ord(c)
+            acc = ((acc << 5) + acc) + code
+        return acc % self.size
+
+    def seek_slot(self, value):
+        slot = self.hash_fun(value)
+        i = slot
+        while self.slots[i] is not None:
+            i = (i + self.step) % self.size
+            if i == slot:
+                return None
+        return i
+
+    def put(self, value):
+        slot = self.seek_slot(value)
+        if slot is not None:
+            self.slots[slot] = value
+            return slot
+        return None
+
+    def find(self, value):
+        i = self.hash_fun(value)
+        while self.slots[i] is not None:
+            if self.slots[i] == value:
+                return i
+            i = (i + self.step) % self.size
+        return None
---
+++ test_lesson8.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import unittest
+from lesson8 import *
+
+
+class TestHashTableMethods(unittest.TestCase):
+
+    def test_hash_function(self):
+        H = HashTable(17, 3)
+        for i in ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF"]:
+            for j in ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF"]:
+                slot = H.hash_fun(i+j)
+                self.assertTrue(slot >= 0)
+                self.assertTrue(slot < H.size)
+                # print("{} = {}".format(i+j, slot))
+        for i in "ABCDEF":
+            self.assertEqual(H.hash_fun([i]*6), 9)
+
+    def test_seek_slot(self):
+        H = HashTable(17, 3)
+        # first found
+        self.assertEqual(H.seek_slot("AAAAAA"), 9)
+        H.slots[9] = "AAAAAA"
+        H.slots[12] = "AAAAAA"
+        H.slots[15] = "AAAAAA"
+        # seek empty slot
+        self.assertEqual(H.seek_slot("AAAAAA"), 1)
+        H.slots[1] = "AAAAAA"
+        H.slots[4] = "AAAAAA"
+        H.slots[7] = "AAAAAA"
+        H.slots[10] = "AAAAAA"
+        H.slots[13] = "AAAAAA"
+        H.slots[16] = "AAAAAA"
+        self.assertEqual(H.seek_slot("AAAAAA"), 2)
+        H.slots[2] = "AAAAAA"
+        H.slots[5] = "AAAAAA"
+        H.slots[8] = "AAAAAA"
+        H.slots[11] = "AAAAAA"
+        H.slots[14] = "AAAAAA"
+        H.slots[0] = "AAAAAA"
+        H.slots[3] = "AAAAAA"
+        H.slots[6] = "AAAAAA"
+        # full
+        self.assertEqual(H.seek_slot("AAAAAA"), None)
+
+    def test_put(self):
+        H = HashTable(17, 3)
+        self.assertEqual(H.put("AAAAAA"), 9)
+        self.assertIsNotNone(H.put("abcdef"))
+        for i in range(17):
+            H.slots[i] = "filled"
+        self.assertIsNone(H.put("AAAAAA"))
+
+    def test_find(self):
+        H = HashTable(17, 3)
+        s1 = H.put("AAAAAA")
+        self.assertEqual(s1, 9)
+        self.assertEqual(s1, H.find("AAAAAA"))
+        self.assertIsNone(H.find("BBBBBB"))
+
+    def test_find2(self):
+        H = HashTable(31, 13)
+        sequence = "abcdefghijklmnopqrstuvwxyz12345"
+        for elem in sequence:
+            self.assertIsNotNone(H.put(elem))
+        sequence2 = ""
+        for elem in sequence:
+            slot = H.find(elem)
+            self.assertIsNotNone(slot)
+            sequence2 += H.slots[slot]
+        self.assertEqual(sequence, sequence2)
+
+
+if __name__ == '__main__':
+    unittest.main()
